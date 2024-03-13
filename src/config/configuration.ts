7f6d91789f47c0fbf8bf8c99b178d68f9604588b
Julien <julien.delphine@epitech.eu>
2024-03-13 16:48:52 +0100

    🔧 feat: update configuration validation and export

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,14 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    uri: process.env.MONGO_URI,
+  },
+  environment: process.env.NODE_ENV,
+  leagueApiKey: process.env.LEAGUE_API_KEY,
+  leagues: {
+    frequency: process.env.LEAGUES_FREQUENCY || 24,
+  },
+  tournaments: {
+    frequency: process.env.TOURNAMENTS_FREQUENCY || 24,
   },
 });
