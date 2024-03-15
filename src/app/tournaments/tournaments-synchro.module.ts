41549855ed98e58e26c999c3764fa1b97409dbf4
Julien Delphine <julien.delphine@epitech.eu>
2024-03-15 16:32:58 +0100

    ✨ feat: implement tournaments synchro service completed

--- src/app/tournaments/tournaments-synchro.module.ts
+++ src/app/tournaments/tournaments-synchro.module.ts
@@ -1,8 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { TournamentSchema } from './schemas/tournament.schema';
 import { TournamentsSynchroService } from './tournaments-synchro.service';
 import { AxiosService } from 'lib/axios/axios.service';
+import { LeagueSchema } from '../leagues/schemas/league.schema';
+import { LeaguesService } from '../leagues/leagues.service';
 
 @Module({})
 export class TournamentsSynchroModule {
@@ -11,11 +14,11 @@ export class TournamentsSynchroModule {
       module: TournamentsSynchroModule,
       imports: [
         MongooseModule.forFeature([
-          { name: 'League', schema: TournamentSchema },
+          { name: 'Tournament', schema: TournamentSchema },
         ]),
+        MongooseModule.forFeature([{ name: 'League', schema: LeagueSchema }]),
       ],
-      providers: [TournamentsSynchroService, AxiosService],
-      exports: [],
+      providers: [TournamentsSynchroService, AxiosService, LeaguesService],
     };
   }
 }
