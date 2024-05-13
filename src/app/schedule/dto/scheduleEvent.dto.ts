522f3a2fab44725f18509443cb8ccfe79bc54973
Julien Delphine <julien.delphine@epitech.eu>
2024-05-13 17:56:20 +0200

    🏷️ chore: update date types for schedule to fit mongoose date object

--- src/app/schedule/dto/scheduleEvent.dto.ts
+++ src/app/schedule/dto/scheduleEvent.dto.ts
@@ -1,6 +1,8 @@
 import { IsString, IsDate } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
+import { Date } from 'mongoose';
+
 import {
   IScheduleEvent,
   IScheduleLeague,
