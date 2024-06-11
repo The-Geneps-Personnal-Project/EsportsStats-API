5327d493ac8802ff38685f0af319cb7c64357669
Julien Delphine <julien.delphine@epitech.eu>
2024-06-11 11:45:15 +0200

    ✅ test(synchro-service): add defined test for live/teams/schedule synchro-service

---
+++ src/app/teams/teams-synchro.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { getModelToken } from '@nestjs/mongoose';
+
+import { TeamsSynchroService } from './teams-synchro.service';
+import { AxiosService } from '../../../lib/axios/axios.service';
+
+describe('TeamsSynchroService', () => {
+  let service: TeamsSynchroService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TeamsSynchroService,
+        AxiosService,
+        ConfigService,
+        SchedulerRegistry,
+        {
+          provide: getModelToken('Team'),
+          useValue: {
+            updateOne: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<TeamsSynchroService>(TeamsSynchroService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
