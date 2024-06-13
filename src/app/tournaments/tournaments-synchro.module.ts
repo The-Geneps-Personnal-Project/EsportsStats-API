0b484f37c6ffe4697f366fc580f3e2e492339551
Julien Delphine <julien.delphine@epitech.eu>
2024-06-13 10:08:38 +0200

    🏗️ chore(tests): add tests folders

--- src/app/tournaments/tournaments-synchro.module.ts
+++ src/app/tournaments/tournaments-synchro.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { TournamentSchema } from './schemas/tournament.schema';
 import { TournamentsSynchroService } from './tournaments-synchro.service';
-import { AxiosService } from 'src/lib/axios/axios.service';
+import { AxiosService } from '../../lib/axios/axios.service';
 import { LeagueSchema } from '../leagues/schemas/league.schema';
 import { LeaguesService } from '../leagues/leagues.service';
 
